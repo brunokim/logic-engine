04d1b0a6abe777e9a7e64985b37ef662ef13f53e
Bruno Kim <brunokim.mc@gmail.com>
2021-05-08 17:42:23 -0300

    Exemplify usage of DecodeInstruction

--- wam/doc_test.go
+++ wam/doc_test.go
@@ -40,3 +40,25 @@ func Example() {
 	// Unordered output: G1 = elizabeth
 	// G2 = philip
 }
+
+func ExampleDecodeInstruction() {
+	instrs := []wam.Instruction{
+		wam.DecodeInstruction(Comp("allocate", Int(2))),
+		wam.DecodeInstruction(Comp("get_struct", Atom("f/2"), Var("X1"))),
+		wam.DecodeInstruction(Comp("switch_on_constant", Dict(
+			Atom("a"), Comp("instr", Ptr(nil), Int(1)),
+			Atom("b"), Comp("instr", Ptr(nil), Int(1))))),
+		wam.DecodeInstruction(Atom("deallocate")),
+		wam.DecodeInstruction(Comp("proceed", Atom("run"))),
+	}
+	for _, instr := range instrs {
+		fmt.Println(instr)
+	}
+	// Output: allocate 2
+	// get_struct f/2, A1
+	// switch_on_constant
+	//	a: <nil>[1]
+	//	b: <nil>[1]
+	// deallocate
+	// proceed run
+}
